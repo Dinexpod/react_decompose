bc240ea86b66c92a9b24df8e833f861f5b87b2a7
dinexpod <work.toxa@gmail.com>
2021-07-10 17:53:53 +0300

    Updates

---
+++ precache-manifest.212c729e6c4cabb210805348a40edf2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583fd9a1d87286ccb8a108e16e4b97bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "031279f1ff8e443a3434",
+    "url": "./static/css/main.c5ce46c6.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031279f1ff8e443a3434",
+    "url": "./static/js/main.5ee31b6b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
